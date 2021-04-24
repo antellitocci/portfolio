a6a5e9387aaacacd91eab93460d566bd757e2f5e
raddymaster <antellitocci@gmail.com>
2021-04-24 17:55:48 -0400

    added javascript to change hero image on hover and bio text

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+function onImageHover(element)
+{
+    element.setAttribute('src', './assets/images/profile-picture.jpg');
+}
+
+function onImageUnHover(element)
+{
+    element.setAttribute('src', './assets/images/profile-picture.png');
+}

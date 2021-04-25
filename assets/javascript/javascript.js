9216f37d829cb49d45b859c81c0a30536208bc78
raddymaster <antellitocci@gmail.com>
2021-04-25 12:11:32 -0400

    addition of photos to my work section and additional styling of my work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,9 +1,11 @@
 function onImageHover(element)
 {
     element.setAttribute('src', './assets/images/profile-picture.jpg');
+    element.style.opacity = "0.75";
 }
 
 function onImageUnHover(element)
 {
     element.setAttribute('src', './assets/images/profile-picture.png');
+    element.style.opacity = "1";
 }
